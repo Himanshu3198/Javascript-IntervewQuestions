fd4437e2ad36dce9456447b68b84efe7aa3cfaef
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-09-24 22:26:51 +0530

    Add files via upload

---
+++ InterviewQuestion.js
@@ -0,0 +1,137 @@
+console.log("himanshu sharma");
+
+// var x=5,y=1  
+// var obj ={ x:10}  
+// with(obj)  
+// {  
+//       alert(y)  
+// }  op:1
+
+
+
+// 3) Which one of the following also known as Conditional Expression:
+
+// a Alternative to if-else
+// b Switch statement
+// c If-then-else statement
+// d immediate if
+// op:d 
+
+
+
+// In JavaScript, what is a block of statement?
+
+// a Conditional block
+// b block that combines a number of statements into a single compound statement
+// c both conditional block and a single statement
+// d block that contains a single statement
+//   op:b
+
+
+
+// var count =0;  
+// while (count <10)  
+// {  
+//      console.log(count);  
+//      count++;  
+// }  
+
+
+
+// var grade='D';  
+// var result;  
+// switch(grade)  
+// {  
+// case'A':  
+//         result+="10";  
+// case'B':  
+//         result+=" 9";  
+// case'C':  
+//         result+=" 8";  
+// case 'D'  
+// result+=" 6";  
+// default:  
+//         result+=" 0";  
+// }  
+// document.write(result);  
+
+
+
+// var grade='Z';  
+// var result;  
+// switch(grade)  
+// {  
+// case'A':  
+//         result+="10";  
+// case'B':  
+//         result+=" 9";  
+// case'C':  
+//         result+=" 8";  
+// default:  
+//         result+=" 0";  
+// }  
+// document.write(result);  
+// op: 0
+
+// 15) Which of the following variables takes precedence over the others if the names are the same?
+
+// a Global variable
+// b The local element
+// c The two of the above
+// d None of the above
+
+// a
+
+// 17) Which of the following type of a variable is volatile?
+
+// a Mutable variable
+// b Dynamic variable
+// c Volatile variable
+// d Immutable variable
+// a 
+
+// In the JavaScript, which one of the following is not considered as an error:
+
+// a Syntax error
+// b Missing of semicolons
+// c Division by zero
+// d Missing of Bracket
+// c 
+
+// Which of the following givenfunctions of the Number Object formats a number with a different number of digits to the right of the decimal?
+
+// a toExponential()
+// b toFixed()
+// c toPrecision()
+// d toLocaleString()
+
+// b
+
+
+// Which of the following number object function returns the value of the number?
+
+// a toString()
+// b valueOf()
+// c toLocaleString()
+// d toPrecision()
+// b 
+
+// Choose the correct snippet from the following to check if the variable "a" is not equal the "NULL":
+
+// a if(a!==null)
+// b if (a!)
+// c if(a!null)
+// d if(a!=null)
+// a 
+
+
+// int x =9;  
+// char y =9;  
+// if(x===y)  
+// return true;  
+// else  
+// return false;  
+
+
+
+
